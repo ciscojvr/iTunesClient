ac31daf6450b57857dd71bb18b0eac0bf7e01c72
Francisco Ozuna <ozuna252@hotmail.com>
2020-03-25 16:29:36 -0400

    Created our data models for the app.

---
+++ iTunesClient/Models/Album.swift
@@ -0,0 +1,13 @@
+//
+//  Album.swift
+//  iTunesClient
+//
+//  Created by Francisco Ozuna on 3/25/20.
+//  Copyright © 2020 ozuna. All rights reserved.
+//
+
+import Foundation
+
+class Album {
+    
+}
---
+++ iTunesClient/Models/Artist.swift
@@ -0,0 +1,23 @@
+//
+//  Artist.swift
+//  iTunesClient
+//
+//  Created by Francisco Ozuna on 3/25/20.
+//  Copyright © 2020 ozuna. All rights reserved.
+//
+
+import Foundation
+
+class Artist {
+    let id: Int
+    let name: String
+    let primaryGenre: Genre
+    var albums: [Album]
+    
+    init(id: Int, name: String, primaryGenre: Genre, albums: [Album]) {
+        self.id = id
+        self.name = name
+        self.primaryGenre = primaryGenre
+        self.albums = albums
+    }
+}
