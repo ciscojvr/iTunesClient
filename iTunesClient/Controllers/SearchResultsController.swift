757099e1a7a7ee1540d5fe8db78dee44d8bc17ad
Francisco Ozuna <ozuna252@hotmail.com>
2020-03-26 15:20:27 -0400

    Added button to present search results table view modally. Implemented search bar in search results table view.

---
+++ iTunesClient/Controllers/SearchResultsController.swift
@@ -0,0 +1,46 @@
+//
+//  SearchResultsController.swift
+//  iTunesClient
+//
+//  Created by Francisco Ozuna on 3/26/20.
+//  Copyright © 2020 ozuna. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultsController: UITableViewController {
+    
+    let searchController = UISearchController(searchResultsController: nil)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(SearchResultsController.dismissSearchResultsController))
+        
+        tableView.tableHeaderView = searchController.searchBar
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchResultsUpdater = self
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    
+    @objc func dismissSearchResultsController() {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SearchResultsController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        print(searchController.searchBar.text!)
+    }
+}
