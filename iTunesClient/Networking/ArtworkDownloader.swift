35546f282ea278fe88cf9e0f4dbeddad979ce9e3
Francisco Ozuna <ozuna252@hotmail.com>
2020-03-27 20:51:14 -0400

    Created ArtworkDownloader to download artwork data.

---
+++ iTunesClient/Networking/ArtworkDownloader.swift
@@ -0,0 +1,43 @@
+//
+//  ArtworkDownloader.swift
+//  iTunesClient
+//
+//  Created by Francisco Ozuna on 3/27/20.
+//  Copyright © 2020 ozuna. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArtworkDownloader: Operation {
+    let album: Album
+    
+    init(album: Album) {
+        self.album = album
+        super.init()
+    }
+    
+    // When the operation enters the ready state and is about to be executed, the operation queue runs the contents of the main function.
+    override func main() {
+        if self.isCancelled {
+            return
+        }
+        
+        guard let url = URL(string: album.artworkUrl) else {
+            return
+        }
+        
+        let imageData = try! Data(contentsOf: url)
+        
+        if self.isCancelled {
+            return
+        }
+        
+        if imageData.count > 0 {
+            album.artwork = UIImage(data: imageData)
+            album.artworkState = .downloaded
+        } else {
+            album.artworkState = .failed
+        }
+    }
+}
